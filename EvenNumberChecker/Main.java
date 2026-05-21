ed8e21cd97cd469a5d0430bbe92aff36f6dba090
AndrewSanAntonio1 <sgandrew290@gmail.com>
2026-05-21 21:42:04 +0800

    Created Even Number Checker using Predicate a list check condition with uses of lambda expression

---
+++ EvenNumberChecker/Main.java
@@ -0,0 +1,10 @@
+package EvenNumberChecker;
+
+import java.util.function.Predicate;
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Integer> isEven = n -> n % 2 == 0;
+
+        System.out.println(isEven.test(10));
+    }
+}
